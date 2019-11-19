6ce5121fbadbe8e52e282963a1cce7a99c576c74
aatthh <54420748+aatthh@users.noreply.github.com>
2019-11-19 15:59:41 -0500

    Update logIn.java

--- logIn.java
+++ logIn.java
@@ -4,26 +4,26 @@
 import javax.swing.*;
 import javax.swing.text.BadLocationException;
 
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.PrintStream;
 import java.util.Date;
 
-class logIn {
-	private static PrintStream standardOut;
-    public static void main(String args[]) {
-
+public class login {
+	
+	
+	public login(){
+	
+		String username;
+		String password;
         //Creating the Frame
         JFrame frame = new JFrame("Know Social");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1280, 720);
+        frame.setSize(300, 500);
 
         //Creating the MenuBar and adding components
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("File");
-        JMenu m2 = new JMenu("Settings");
-        mb.add(m1);
-        mb.add(m2);
+        
         JMenuItem m22 = new JMenuItem("About");
         JMenuItem m33 = new JMenuItem("Quit");
         m1.add(m33);
@@ -33,8 +33,8 @@ class logIn {
         JPanel panel = new JPanel(); // the panel is not visible in output
         JLabel user = new JLabel("Username",SwingConstants.CENTER);
         JLabel pwd = new JLabel("Password");
-        JTextField tf = new JTextField(10); // accepts up to 10 characters
-        JTextField tf2 = new JTextField(10); // accepts up to 10 characters
+        JTextField tf = new JTextField(20); // accepts up to 10 characters
+        JTextField tf2 = new JTextField(20); // accepts up to 10 characters
         JButton send = new JButton("Login");
         panel.add(user); // Components Added using Flow Layout
         panel.add(tf); // Components Added using Flow Layout
@@ -46,8 +46,9 @@ class logIn {
         m33.addActionListener(new ActionListener() {
         	  public void actionPerformed(ActionEvent event) {
         	    System.exit(0);
+        	  
         	  }
-        	});
+        	  });
         //Action for About Button
         m22.addActionListener(new ActionListener() {
         	  public void actionPerformed(ActionEvent event) {
@@ -60,9 +61,9 @@ class logIn {
         ta.setEditable(false);
         
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        //frame.getContentPane().add(BorderLayout.CENTER, ta);
         frame.setVisible(true);
     }
+
 }
