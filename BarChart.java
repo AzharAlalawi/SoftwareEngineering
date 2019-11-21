3305902dba2e96f1c67c9fa19b9f99430783025f
Joey Thompson <joeythompson1014@gmail.com>
2019-11-21 17:37:09 -0500

    Update BarChart.java

--- BarChart.java
+++ BarChart.java
@@ -1,6 +1,5 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -13,6 +12,7 @@ import org.jfree.chart.labels.ItemLabelAnchor;
 import org.jfree.chart.labels.ItemLabelPosition;
 import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
 import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.renderer.category.CategoryItemRenderer;
 import org.jfree.chart.ui.TextAnchor;
 import org.jfree.data.category.DefaultCategoryDataset;
@@ -38,31 +38,38 @@ public class BarChart {
 		
 		//Get Chart Data
 		DefaultCategoryDataset barChartData = new DefaultCategoryDataset();
-		barChartData.setValue(66,"Sentiment Percentage","Positive");
-		barChartData.setValue(34,"Sentiment Percentage","Negative");
+		barChartData.setValue(74,"Positive Sentiment","Positive");
+		barChartData.setValue(26,"Negative Sentiment","Negative");
 		
 		//Create Chart
-		JFreeChart barChart = ChartFactory.createBarChart("Overall Sentiment", "Sentiment", "Sentiment Percentage ( % )",barChartData);
+		JFreeChart barChart = ChartFactory.createBarChart("Overall Sentiment for Keyword", "Sentiment", "Sentiment Percentage ( % )",barChartData);
 		CategoryPlot barChrt = barChart.getCategoryPlot();
-		barChrt.setRangeGridlinePaint(Color.red);
+		barChrt.setRangeGridlinePaint(Color.white);
+		
 		
 		//Place Percentages on Top of Bars
-		CategoryItemRenderer renderer = ((CategoryPlot)barChart.getPlot()).getRenderer();
-		renderer.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
-		renderer.setDefaultItemLabelsVisible(true);
+		CategoryItemRenderer renderers = ((CategoryPlot)barChart.getPlot()).getRenderer();
+		renderers.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+		renderers.setDefaultItemLabelsVisible(true);
 		ItemLabelPosition position = new ItemLabelPosition(ItemLabelAnchor.OUTSIDE12,
 		        TextAnchor.TOP_CENTER);
-		renderer.setDefaultPositiveItemLabelPosition(position);
+		renderers.setDefaultPositiveItemLabelPosition(position);
+		
+		CategoryPlot plot = barChrt.getChart().getCategoryPlot();
+		BarRenderer renderer = (BarRenderer) plot.getRenderer();
+
+		// set the color (r,g,b) or (r,g,b,a)
+		Color color = new Color(79, 129, 189);
+		renderer.setSeriesPaint(0, color.green);
+		renderer.setSeriesPaint(1, color.red);
 		
 		//Add Bar Chart to JFrame
 		ChartPanel barPanel = new ChartPanel(barChart);
 		topPanel.removeAll();
 		topPanel.add(barPanel,BorderLayout.CENTER);
-
 		
 		//Show The Window
 		window.setVisible(true);
-		
 		
 	}
 	
