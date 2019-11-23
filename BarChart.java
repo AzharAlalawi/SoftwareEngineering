8e3d7a35a14396864d912ae077cd872bcf069573
Joey Thompson <joeythompson1014@gmail.com>
2019-11-23 14:52:25 -0500

    Update BarChart.java

--- BarChart.java
+++ BarChart.java
@@ -1,7 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -18,59 +23,74 @@ import org.jfree.chart.ui.TextAnchor;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class BarChart {
-	
+
 	public static void main(String[] args) {
-		
-		//Create & Configure Window
+
+		// Create & Configure Window
 		JFrame window = new JFrame();
 		window.setTitle("Know Social");
-		window.setSize(720,480);
+		window.setSize(720, 480);
 		window.setLayout(new BorderLayout());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Create Drop-down Box & Place at Top of Window
-		JComboBox<String> list = new JComboBox<String>();
-		JButton runButton = new JButton("Run Analysis");
+		//Get the screen size  
+		Toolkit toolkit = Toolkit.getDefaultToolkit();  
+		Dimension screenSize = toolkit.getScreenSize(); 
+		  //Calculate the frame location
+		  int x = (screenSize.width - window.getWidth()) / 2;  
+		  int y = (screenSize.height - window.getHeight()) / 2; 
+		  
+		  //Set the new frame location  
+		  window.setLocation(x, y); 
+
+		// Create Drop-down Box & Place at Top of Window
 		JPanel topPanel = new JPanel();
-		topPanel.add(list);
-		topPanel.add(runButton);
-		window.add(topPanel,BorderLayout.NORTH);
-		
-		//Get Chart Data
+		window.add(topPanel, BorderLayout.CENTER);
+
+		// Get Chart Data
 		DefaultCategoryDataset barChartData = new DefaultCategoryDataset();
-		barChartData.setValue(74,"Positive Sentiment","Positive");
-		barChartData.setValue(26,"Negative Sentiment","Negative");
-		
-		//Create Chart
-		JFreeChart barChart = ChartFactory.createBarChart("Overall Sentiment for Keyword", "Sentiment", "Sentiment Percentage ( % )",barChartData);
+		barChartData.setValue(74, "Positive Sentiment", "Positive");
+		barChartData.setValue(26, "Negative Sentiment", "Negative");
+
+		// Create Chart
+		JFreeChart barChart = ChartFactory.createBarChart("Overall Sentiment for Keyword is Positive w/Stanford Core NLP", "Sentiment",
+				"Sentiment Percentage ( % )", barChartData);
 		CategoryPlot barChrt = barChart.getCategoryPlot();
-		barChrt.setRangeGridlinePaint(Color.white);
-		
-		
-		//Place Percentages on Top of Bars
-		CategoryItemRenderer renderers = ((CategoryPlot)barChart.getPlot()).getRenderer();
+		barChrt.setRangeGridlinePaint(Color.WHITE);
+
+		// Pull Image and Set Background
+		BufferedImage image = null;
+		File url = new File("path to bg");
+		try {
+			image = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		barChart.setBackgroundImage(image);
+		barChrt.setBackgroundImage(image);
+
+		// Place Percentages on Top of Bars
+		CategoryItemRenderer renderers = ((CategoryPlot) barChart.getPlot()).getRenderer();
 		renderers.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
 		renderers.setDefaultItemLabelsVisible(true);
-		ItemLabelPosition position = new ItemLabelPosition(ItemLabelAnchor.OUTSIDE12,
-		        TextAnchor.TOP_CENTER);
+		ItemLabelPosition position = new ItemLabelPosition(ItemLabelAnchor.OUTSIDE12, TextAnchor.TOP_CENTER);
 		renderers.setDefaultPositiveItemLabelPosition(position);
-		
+
 		CategoryPlot plot = barChrt.getChart().getCategoryPlot();
 		BarRenderer renderer = (BarRenderer) plot.getRenderer();
 
-		// set the color (r,g,b) or (r,g,b,a)
+		// Set the color (r,g,b) or (r,g,b,a)
 		Color color = new Color(79, 129, 189);
-		renderer.setSeriesPaint(0, color.green);
-		renderer.setSeriesPaint(1, color.red);
-		
-		//Add Bar Chart to JFrame
+		renderer.setSeriesPaint(0, color.GREEN);
+		renderer.setSeriesPaint(1, color.RED);
+
+		// Add Bar Chart to JFrame
 		ChartPanel barPanel = new ChartPanel(barChart);
 		topPanel.removeAll();
-		topPanel.add(barPanel,BorderLayout.CENTER);
-		
-		//Show The Window
+		topPanel.add(barPanel, BorderLayout.CENTER);
+
+		// Show The Window
 		window.setVisible(true);
-		
+
 	}
-	
+
 }
